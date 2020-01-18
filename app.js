b0c4283dd50d8fa0eba6c57c1d03a267a0eab5d8
shabeersha <shabeersha786786@gmail.com>
2020-01-19 00:34:15 +0530

    DAY3: database configuration is done

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
+
+const app=express();
+
+//Config Mongoose
+
+mongoose.connect("mongodb://localhost:27017/cmsDB",{ useNewUrlParser: true })
+    .then( response =>{
+        console.log("MongoDB connected Successfully");
+    }).catch( err=>{
+        console.log("Database Connection Failed");
+});
+
+
+
+/* Configure Express*/
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+/* Routes */
+
+app.use('/',(req,res) =>{
+        res.send("welcome to cms");
+});
+
+
+
+
+
+
+app.listen(3000,() =>{
+    console.log("Server is Running on Port 3000");
+});
